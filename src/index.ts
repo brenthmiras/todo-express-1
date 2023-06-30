49772e7106d32754e7f65f903b2b7c0dcbfa1f54
Brenth Miras <brenthmiras2@gmail.com>
2023-06-30 08:44:07 +0800

    Setup local env

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import { app } from './app';
+import mongoose from 'mongoose';
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
-});
+const start = async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
+  await mongoose.connect(process.env.MONGO_URI);
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!');
+  });
+};
+
+start();
