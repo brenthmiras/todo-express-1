49db412819c4dfff24a04ee8257c4d5a09521c21
Brenth Miras <brenthmiras2@gmail.com>
2023-06-30 12:13:16 +0800

    PUT /api/todos/:id - Create route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 import { listTodoRouter } from './controllers/list-todo';
 import { errorHandler } from './middlewares/error-handler';
 import { newTodoRouter } from './controllers/new-todo';
+import { updateTodoRouter } from './controllers/update-todo';
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.use(json());
  */
 app.use(listTodoRouter);
 app.use(newTodoRouter);
+app.use(updateTodoRouter);
 
 if (process.env.NODE_ENV === 'development') {
   app.use('/apidoc', express.static(__dirname + '/apidoc'));
---
+++ src/controllers/update-todo.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { Todo } from '../models/todo';
+import { validateRequest } from '../middlewares/validate-request';
+import { NotFoundError } from '../errors/not-found-error';
+
+const router = express.Router();
+
+/**
+ * @api {put} /api/todos/:id Update todos
+ * @apiName PutTodos
+ * @apiGroup Todo
+ *
+ * @apiParam {String} title The todo's title
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "title": "New Title",
+ *         "completed": false,
+ *         "_id": "649e517aee5fac84a812752a",
+ *         "__v": 0
+ *     }
+ */
+router.put(
+  '/api/todos/:id',
+  [body('title').isString().withMessage('Title is required')],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const todo = await Todo.findById(req.params.id);
+
+    if (!todo) {
+      throw new NotFoundError();
+    }
+
+    todo.set({
+      title: req.body.title,
+    });
+
+    await todo.save();
+
+    res.status(200).send(todo);
+  }
+);
+
+export { router as updateTodoRouter };
